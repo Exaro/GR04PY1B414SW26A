13739dc6105498746bfa904850f00361cd5a2328
Exaro <alfredorv2302@gmail.com>
2026-05-21 01:47:54 -0500

    Añadiendo archivo .jar

--- invt/src/main/java/com/example/MainApp/Launcher.java
+++ invt/src/main/java/com/example/MainApp/Launcher.java
@@ -2,7 +2,6 @@ package com.example.MainApp;
 
 public class Launcher {
     public static void main(String[] args) {
-        // Llama de forma directa al MainApp de JavaFX
         MainApp.main(args);
     }
 }
